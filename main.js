2c6601b9b59e0da1b5371036fff9fa69a821e5c2
jef210 <jmalek210@gmail.com>
2015-10-16 01:16:04 -0600

    added responsiveness, colors, working nav collapse, and ids, added te and email links

--- main.js
+++ main.js
@@ -6,27 +6,7 @@ $(document).ready(function(){
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
 
-    // Toggle the section content 
-    $('#about-text').hide();
-    $('.about-section').on('click', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $('#about-text').fadeToggle('slow');
-    });
-    
-    $('#examples-text').hide();
-    $('.examples-section').on('click', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $('#examples-text').fadeToggle();
-    }); 
-    
-    $('#what-i-do-text').hide();
-    $('.what-i-do-section').on('click', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $('#what-i-do-text').fadeToggle();
-    }); 
+    // Toggle the section content
     
     $('#data-extra-text').hide();
     $('.data-section').on('click', function(e){
